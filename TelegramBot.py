7f582e7e1b171a998281de135acbf370b30b2bb8
Timur0798 <162792932+Timur0798@users.noreply.github.com>
2024-04-08 16:16:29 +0300

    Changing 8/04/2024-Working version

--- TelegramBot.py
+++ TelegramBot.py
@@ -1,22 +1,52 @@
 import telebot
 import requests
-
+import json
 
 TOKEN = "7177889986:AAHN7_vhXglbEyDlGSXSQY2fq40-25lZ_zE"
 chat_id="5425211469"
 mybot = telebot.TeleBot(TOKEN)
 
-instruction="ИНСТРУКЦИЯ"
+instruction=("Для начала работы введите данные (без склонения) в следующем формате:\n<название валюты> <в какую валюту конвертировать>\
+<количество валюты>\n<посмотреть список доступных валют /values>\n<помощь в работе /help>")
 
 keys={"рубль":"RUB",
       "доллар":"USD",
       "евро":"EUR"
       }
 
+class APIException(Exception):
+    pass
+
+class MoneyConverter:
+    @staticmethod
+    def get_price(quote=str,base=str,amount=str):
+        if quote == base:
+            raise APIException(f"Нельзя конвертировать валюту {base} саму в себя")
+
+        try:
+            quote_ticker = keys[quote]
+        except KeyError:
+            raise APIException(f"Неправильный ввод валюты {quote}")
+
+        try:
+            base_ticker = keys[base]
+        except KeyError:
+            raise APIException(f"Неправильный ввод валюты {base}")
+
+        try:
+            amount = float(amount)
+        except ValueError:
+            raise APIException(f"Не удалось распознать количество {amount}")
+
+        getter = requests.get(f"https://v6.exchangerate-api.com/v6/f7a3baf2ae0567e776cf4972/pair/{quote_ticker}/{base_ticker}")
+        result=json.loads(getter.content)["conversion_rate"]
+        return result
+
+
 @mybot.message_handler(commands=["start"])
 def start(message):
     name = message.from_user.first_name
-    text=f"Здравствуй, {name}!\n {instruction}"
+    text=f"Здравствуйте, {name}!\n {instruction}"
     mybot.reply_to(message,text)
 
 @mybot.message_handler(commands=["help"])
@@ -34,6 +64,19 @@ def values(message):
     mybot.reply_to(message,text)
 
 
+# основной рабочий модуль с динамической ссылкой
+@mybot.message_handler(content_types=["text"])
+def convertation(message):
+    objects = message.text.split(" ")
+
+    if len(objects) != 3:
+        raise ("Вы ввели большее количество параметров")
+
+    quote, base, amount = objects
+    result=MoneyConverter.get_price(quote,base,amount)
+
+    text = f"Цена {amount} {quote} равна {float(amount)*result} {base}"
+    mybot.reply_to(message, text)
 
 
 mybot.polling(none_stop=True)
--- test.py
+++ test.py
@@ -1,4 +1,13 @@
 import requests
+import json
 
-r=requests.get("https://v6.exchangerate-api.com/v6/f7a3baf2ae0567e776cf4972/pair/EUR/RUB")
-print(r.content)
+message=input("Введите:")
+keys={"рубль":"RUB",
+      "доллар":"USD",
+      "евро":"EUR"
+      }
+quote,base,quantity=message.split(" ")
+
+getter=requests.get(f"https://v6.exchangerate-api.com/v6/f7a3baf2ae0567e776cf4972/pair/{keys[quote]}/{keys[base]}")
+a=json.loads(getter.content)["conversion_rate"]
+print(a)
